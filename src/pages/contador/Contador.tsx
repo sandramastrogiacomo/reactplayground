5225bc0c9f130e90ac2b32961c2ab4786b67b5f0
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-04-04 11:22:44 -0300

    React Playground

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,22 @@
+﻿import { useState } from "react";
+
+function Contador() {
+ // let numero = 0
+const [numero, setNumero] = useState (0)
+
+  function somar () {
+    setNumero (numero + 1)
+
+  }
+  
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+      <p> número atual: {numero} </p>
+      <button onClick = {somar}>Aumentar +1</button>
+
+    </div>
+  )
+}
+
+export default Contador
